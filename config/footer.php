c0235c90aec2271fcf5b553f51629cb065e1e58a
wesley soares de souza <wesley.professor.ifsp@gmail.com>
2020-04-24 14:17:02 -0300

    alteração footer

--- config/footer.php
+++ config/footer.php
@@ -1,4 +1,4 @@
-    <center><h6>IFSP - VOTUPORANGA @2019</h6></center>
+    <center><h6>IFSP - VOTUPORANGA @2020</h6></center>
     <center><h6>PROJETO INTERDISCIPLINAR</h6></center>
 </div>
 </body>
